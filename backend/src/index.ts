c5ca3ca0afd437b301c8001b118fe973c06f3b40
manueljsanchezg <manueljsanchez10@gmail.com>
2026-02-12 23:20:16 +0100

    feat: Addes Hono rate limiting

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,21 @@
 import { Hono } from 'hono'
 import { cors } from 'hono/cors'
-
+import { rateLimiter } from "hono-rate-limiter";
 import { authRoutes } from './auth/auth.routes'
 import { tripRoutes } from './trip/trip.routes'
 import { userRoutes } from './user/user.routes'
 import { citiesRoutes } from './cities/cities.routes'
 
 const app = new Hono()
+
+const limiter = rateLimiter({
+  windowMs: 5 * 60 * 1000,
+  limit: 100,
+  standardHeaders: "draft-6",
+  keyGenerator: (c) => c.req.header("x-forwarded-for") || "",
+})
+
+app.use(limiter)
 
 app.use(
   '/api/*',
