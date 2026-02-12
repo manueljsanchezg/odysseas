40161826bc0fd8ef19a0bf09a359a6253cb51286
manueljsanchezg <manueljsanchez10@gmail.com>
2026-02-13 00:24:27 +0100

    feat: update pool conection with ssl

--- backend/src/infra/db.ts
+++ backend/src/infra/db.ts
@@ -1,7 +1,15 @@
 import 'dotenv/config'
 
 import { drizzle } from 'drizzle-orm/node-postgres'
+import { Pool } from 'pg'
 
-const db = drizzle(process.env.DATABASE_URL!)
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL!,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+})
+
+const db = drizzle(pool)
 
 export default db
